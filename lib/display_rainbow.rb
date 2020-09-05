db7adf89f09720a6a216139950079d7ee3648287
Aaron Amodt <aaron@amodttech.com>
2020-09-05 01:27:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-letters(['R', ])
-
+display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+letters = ["R", "O", "Y", "G", "B", "I", "V"]
 
 def display_rainbow
-  
+  puts "R:"
 end
